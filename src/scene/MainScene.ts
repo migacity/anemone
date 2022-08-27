245a7c8ad2148fef97bda01c19c8970dd9a05bd7
idomshi <56523771+idomshi@users.noreply.github.com>
2022-08-27 16:09:03 +0900

    ESLintに怒られたので型を書いたりしました

--- src/scene/MainScene.ts
+++ src/scene/MainScene.ts
@@ -15,13 +15,14 @@ export class MainScene extends Phaser.Scene {
     const { width, height } = this.game.canvas;
     this.add.image(width / 2, height / 2, "mainImage").setScale(2, 2);
     this.scene.launch("inputManager");
-    this.dialog = new MessageWindow(this)
+    this.dialog = new MessageWindow(this);
     this.add.existing(this.dialog);
-    setTimeout(() =>
-      this.dialog.setMessage(
-        '拙者親方と申すは、御立会の内に御存知の御方も御座りましょうが、御江戸を発って二十里上方、相州小田原一色町を御過ぎなされて、青物町を上りへ御出でなさるれば、欄干橋虎屋藤右衛門、只今では剃髪致して圓斎と名乗りまする。'
-      ),
+    setTimeout(
+      () =>
+        this.dialog.setMessage(
+          "拙者親方と申すは、御立会の内に御存知の御方も御座りましょうが、御江戸を発って二十里上方、相州小田原一色町を御過ぎなされて、青物町を上りへ御出でなさるれば、欄干橋虎屋藤右衛門、只今では剃髪致して圓斎と名乗りまする。"
+        ),
       800
-    )
+    );
   }
 }
