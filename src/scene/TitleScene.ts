87b9a49551bc66154607b2678e9078b22bead18b
idomshi <56523771+idomshi@users.noreply.github.com>
2022-11-12 14:58:48 +0900

    Scene.eventsを使いたいけど書き直しが広範囲っぽくてあんまり乗り気じゃないです

--- src/scene/TitleScene.ts
+++ src/scene/TitleScene.ts
@@ -13,5 +13,10 @@ export class TitleScene extends Phaser.Scene {
     const { width, height } = this.game.canvas;
     this.add.image(width / 2, height / 2, "titleImage").setScale(2, 2);
     this.scene.launch("inputManager");
+    this.events.on('click', this.moveNext, this)
+  }
+
+  moveNext(): void {
+
   }
 }
