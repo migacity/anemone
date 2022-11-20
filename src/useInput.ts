8378682f00adcaf36966ee8e6da28ab7d3851ab6
idomshi <56523771+idomshi@users.noreply.github.com>
2022-11-20 15:35:40 +0900

    関数の返り値の型をめいきするなどしました

--- src/useInput.ts
+++ src/useInput.ts
@@ -1,25 +1,33 @@
-export const useInput = (scene: Phaser.Scene) => {
-  const self = scene
+interface UseInput {
+  zone: Phaser.GameObjects.Zone;
+  setEventHandler: (handler: Function) => void;
+  removeEventHandler: (handler: Function) => void;
+}
+
+export const useInput = (scene: Phaser.Scene): UseInput => {
+  const self = scene;
   const { width, height } = self.game.canvas;
-  const zone = self.add.zone(width / 2, height / 2, width, height)
+  const zone = self.add.zone(width / 2, height / 2, width, height);
   zone.setInteractive({
     useHandCursor: true,
-  })
-  const space = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+  });
+  const space = scene.input.keyboard.addKey(
+    Phaser.Input.Keyboard.KeyCodes.SPACE
+  );
 
-  const setEventHandler = (handler: Function) => {
-    zone.on('pointerdown', handler, self)
-    space.on('down', handler, self)
-  }
+  const setEventHandler = (handler: Function): void => {
+    zone.on("pointerdown", handler, self);
+    space.on("down", handler, self);
+  };
 
-  const removeEventHandler = (handler: Function) => {
-    zone.off('pointerdown', handler, self)
-    space.off('down', handler, self)
-  }
+  const removeEventHandler = (handler: Function): void => {
+    zone.off("pointerdown", handler, self);
+    space.off("down", handler, self);
+  };
 
   return {
     zone,
     setEventHandler,
     removeEventHandler,
-  }
-}
+  };
+};
