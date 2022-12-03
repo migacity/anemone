eabb71408de690ebb8037f3148916e74f1f1817a
idomshi <56523771+idomshi@users.noreply.github.com>
2022-12-03 22:05:00 +0900

    formatし忘れていたファイルがありました

--- src/scene/EndingScene.ts
+++ src/scene/EndingScene.ts
@@ -14,11 +14,11 @@ export class EndingScene extends Phaser.Scene {
     const { width, height } = this.game.canvas;
     this.add.image(width / 2, height / 2, "endingImage").setScale(2, 2);
 
-    const { setEventHandler } = useInput(this)
-    setEventHandler(this.moveNext)
+    const { setEventHandler } = useInput(this);
+    setEventHandler(this.moveNext);
   }
 
   moveNext(): void {
-    this.scene.start('credit')
+    this.scene.start("credit");
   }
 }
--- src/scene/TitleScene.ts
+++ src/scene/TitleScene.ts
@@ -14,11 +14,11 @@ export class TitleScene extends Phaser.Scene {
     const { width, height } = this.game.canvas;
     this.add.image(width / 2, height / 2, "titleImage").setScale(2, 2);
 
-    const { setEventHandler } = useInput(this)
-    setEventHandler(this.moveNext)
+    const { setEventHandler } = useInput(this);
+    setEventHandler(this.moveNext);
   }
 
   moveNext(): void {
-    this.scene.start('main')
+    this.scene.start("main");
   }
 }
