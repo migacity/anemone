53ca79efff56a5cfe221ff25178cf72fdbf5f65d
idomshi <56523771+idomshi@users.noreply.github.com>
2023-02-04 21:46:10 +0900

    シナリオの状態をlocalstorageに保存する処理を削除します

--- src/scene/MainScene.ts
+++ src/scene/MainScene.ts
@@ -2,15 +2,8 @@ import mainImage from "../../assets/main.webp";
 import { MessageWindow } from "./MessageWindowScene";
 import { useInput } from "../useInput";
 import { scenario, preload } from "../scenario";
-// import { GameObjects } from "phaser";
-import { load } from "../dataSaver";
 import { ButtonOption, useUi } from "../uiManager";
 import { store } from "../useState";
-
-// interface CharData {
-//   name: string;
-//   key: string;
-// }
 
 export class MainScene extends Phaser.Scene {
   // 出来ればundefinedは無い方がいい。
@@ -42,11 +35,8 @@ export class MainScene extends Phaser.Scene {
   }
 
   async create(): Promise<void> {
-    console.log(store.get());
-    console.log(scenario[store.get().part][store.get().chapter]);
     this.scenarioIndex = -1;
     const { width, height } = this.game.canvas;
-    const gameData = load();
 
     // 真っ黒な画面から始める。
     this.cameras.main.fadeOut(20);
@@ -56,25 +46,11 @@ export class MainScene extends Phaser.Scene {
     this.bg = this.add.image(
       width / 2,
       height / 2,
-      gameData?.datas[0].bg ?? "mainImage"
+      "mainImage"
     );
 
     // 立ち絵用のコンテナを用意する。
     this.character = this.add.container(width / 2, height / 2);
-
-    // 立ち絵データがあればロードする。
-    (gameData?.datas[0].character ?? []).forEach(({ name, key }) => {
-      const char = this.character.getByName(name);
-      if (!(char instanceof Phaser.GameObjects.Image) && !(char === null))
-        return;
-      if (char === null) {
-        const image = this.add.image(0, 0, key);
-        image.setName(name);
-        this.character.add(image);
-      } else {
-        char.setTexture(key);
-      }
-    });
 
     // メッセージウィンドウを表示する。
     this.dialog = new MessageWindow(this);
@@ -137,8 +113,8 @@ export class MainScene extends Phaser.Scene {
     ]);
 
     // シナリオが自動的に始まるように。
-    this.scenarioIndex = gameData?.datas[0].scenarioIndex ?? -1;
-    if (!(gameData?.datas[0].camera ?? true)) this.cameras.main.fadeIn(1000);
+    this.scenarioIndex = -1;
+    this.cameras.main.fadeIn(1000);
     await this.onClick();
   }
 
@@ -158,31 +134,6 @@ export class MainScene extends Phaser.Scene {
     this.scenarioIndex = -1;
     this.scene.start(sceneName);
   }
-
-  // /** 表示しているシナリオの状態をconsoleに吐く。 */
-  // private printParams(): void {
-  //   const character = this.character.list
-  //     .map((v: GameObjects.GameObject): CharData | undefined => {
-  //       if (!(v instanceof GameObjects.Image)) return undefined;
-  //       return { name: v.name, key: v.texture.key };
-  //     })
-  //     .filter((item): item is NonNullable<typeof item> => item !== undefined);
-
-  //   const params: SaveData = {
-  //     scenarioIndex: this.scenarioIndex - 1,
-  //     bg: this.bg.texture.key,
-  //     character,
-  //     code: scenario[store.get().part][store.get().chapter][this.scenarioIndex],
-  //     camera: this.cameras.main.fadeEffect.direction,
-  //   };
-
-  //   if (import.meta.env.MODE === "development") console.log(params);
-  //   save({
-  //     monologue1Viewed: false,
-  //     monologue2Viewed: false,
-  //     datas: [params],
-  //   });
-  // }
 
   async interpretation(): Promise<void> {
     do {
@@ -224,7 +175,6 @@ export class MainScene extends Phaser.Scene {
           this.moveNext(code.to, code.sceneName);
           break;
       }
-      // this.printParams();
     } while (
       this.scenarioIndex < 0 ||
       (scenario[store.get().part][store.get().chapter][this.scenarioIndex]
