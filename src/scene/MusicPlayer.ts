07ffd97584e9f86028c16808e861c19166e7255f
idomshi <56523771+idomshi@users.noreply.github.com>
2023-03-25 14:12:04 +0900

    戻るボタンの文字をほかに合わせました

--- src/scene/MusicPlayer.ts
+++ src/scene/MusicPlayer.ts
@@ -130,13 +130,19 @@ export class MusicPlayer extends Phaser.Scene {
 
     // 戻るボタンを追加する。
     const i = this.musics.length;
+    const text = this.add.text(0, 0, "戻る", {
+      fontSize: "24px",
+      padding: { top: 4 },
+    });
+    const caption = this.add.container(undefined, undefined, [text]);
+    text.setOrigin(0.5, 0.5);
     buttons.push({
-      type: "textButton",
+      type: "containerButton",
       top: Math.floor(i / 2) * dh - (dh * 3) / 2,
       left: (i % 2) * (2 * dw - w) + (w / 2 - dw),
       width: w,
       height: h,
-      caption: "戻る",
+      caption,
       onClick: () => {
         this.bgm?.stop();
         resetCounter();
